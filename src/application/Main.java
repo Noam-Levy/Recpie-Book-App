901a18293c7e5f27f3588a866d176657ef8098d3
Noam Levy <90918336+Noam-Levy@users.noreply.github.com>
2021-12-31 14:05:34 +0200

    GUI work (#1)
    * Initial branch data
    
    * Keep original build.fxbuild file
    
    * Keep original Main.class
    
    * Keep original module-info.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,9 @@
 package application;
 	
-import java.net.ConnectException;
-
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
-import model.RecipeFetcher;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
@@ -13,26 +12,17 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Parent root =FXMLLoader.load(getClass().getResource("menu.fxml"));
+			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
 	public static void main(String[] args) {
-		try {
-			RecipeFetcher.getInstance("").getRandomRecipe();
-			//RecipeFetcher.getInstance().convertMeasurementToGrams(null);
-		} catch (ConnectException e) {
-			System.err.println("Cannot connect to API. Check internet connection");
-		}
-		catch (Exception e) {
-			System.err.println(e.getMessage());;
-		}
-		//launch(args);
+		launch(args);
 	}
 }
--- src/module-info.java
+++
@@ -1,8 +0,0 @@
-module OODFinalProject_AmitMaudi_NoamLevy {
-	requires javafx.controls;
-	requires javafx.graphics;
-	requires java.net.http;
-	requires json.simple;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
