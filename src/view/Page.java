c476ec31ff90a893bd73cf9f58f282adee25e52d
Noam Levy <90918336+Noam-Levy@users.noreply.github.com>
2022-01-02 19:51:47 +0200

    Update Page.java
    showErrorWindow function should not be deleted. this is our way to convey exceptions to the user if needed.

--- src/view/Page.java
+++ src/view/Page.java
@@ -10,10 +10,21 @@ import listeners.UIEventListener;
 
 public abstract class Page extends Pane  {
 	
-	private static ArrayList<UIEventListener> listeners = new ArrayList<UIEventListener>();
+	protected static ArrayList<UIEventListener> listeners = new ArrayList<UIEventListener>();
 	
 	
 	public final void addListener(UIEventListener listener) {
 		listeners.add(listener);
+	}
+	
+	public final void showErrorWindow(String error) {
+		Stage stage = new Stage();
+		stage.setTitle("Something went wrong");
+		StackPane root = new StackPane();
+		Label label = new Label(error);
+		label.setTextFill(Color.RED);
+		root.getChildren().add(label);
+		stage.setScene(new Scene(root, 300, 50));
+		stage.show();
 	}
 }
