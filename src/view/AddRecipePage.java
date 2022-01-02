a8f4f6f9ab7a617b2dae12b5cff2b5b8b526ccb0
Noam Levy <90918336+Noam-Levy@users.noreply.github.com>
2022-01-02 19:46:58 +0200

    overall ordering and cleaning
    * correcting typos
    * cleaning the code
    * documentation
    * new comments needs to be checked!

--- src/view/AddRecipePage.java
+++ src/view/AddRecipePage.java
@@ -16,31 +16,28 @@ import javafx.scene.layout.GridPane;
 
 public class AddRecipePage extends Page implements Initializable {
 
-	@FXML
-	private GridPane GPingiridiant;
+	@FXML private GridPane GPingiridiant;
+	@FXML private GridPane GPinstruction;
+	@FXML private Button btnAddRowIngrediant;
+	@FXML private Button btnAddRowInstruction;
+	@FXML private ChoiceBox<String> cmbUnit;
+	@FXML private TextField txfRecipieCookTime, txfRecipieCuisine, txfRecipieName;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// needs to be changed according to real runtime enviroment
+		cmbUnit.getItems().add("1");
+		cmbUnit.getItems().add("2");
+		cmbUnit.getItems().add("3");		
+	}
 
 	@FXML
-	private GridPane GPinstruction;
-
-	@FXML
-	private Button btnAddRowIngrediant;
-
-	@FXML
-	private Button btnAddRowInstruction;
-	@FXML
-	private ChoiceBox<String> cmbUnit;
-
-
-
-	@FXML
-	void addRowIngrediant(ActionEvent event) {
-		// need to check if all filled before make new row
-
-		int allIndexes = GPingiridiant.getRowCount()* GPingiridiant.getColumnCount();
-		if (!checkGPingiridiant(4,allIndexes))
+	void addRowIngredient(ActionEvent event) {
+		// checks if all opened ingredients rows are filled before allowing to add more
+		int allIndexes = GPingiridient.getRowCount()* GPingiridiant.getColumnCount();
+		if (!checkGPingiridient(4,allIndexes))
 			return;
-	
-		
+		// create new choice box and add items. NEEDS TO BE CHAGNED FROM STRING TO INGREDIENT
 		ChoiceBox<String> cmbUnitClone = new ChoiceBox<String>();
 		for (String s : cmbUnit.getItems()) {
 			cmbUnitClone.getItems().add(s);	
@@ -51,41 +48,19 @@ public class AddRecipePage extends Page implements Initializable {
 
 	@FXML
 	void addRowInstruction(ActionEvent event) {
+		// checks if all opened instructions rows are filled before allowing to add more
 		int allIndexes = GPinstruction.getRowCount()* GPinstruction.getColumnCount();
-		
 		if (!checkGPinstruction(0,allIndexes))
 			return;
-		
 		GPinstruction.addRow(GPinstruction.getRowCount(), new Label(GPinstruction.getRowCount()+""),new TextField());
 	}
 
-	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {
-
-		cmbUnit.getItems().add("1");
-		cmbUnit.getItems().add("2");
-		cmbUnit.getItems().add("3");		
-	}
-
-	@FXML
-	private TextField txfRecipieCookTime;
-
-	@FXML
-	private TextField txfRecipieCuisine;
-
-	@FXML
-	private TextField txfRecipieName;
-
-
-
 	@FXML
 	void saveRecipie(ActionEvent event) {
-
 		if (txfRecipieCookTime.getText().isEmpty() || txfRecipieCuisine.getText().isEmpty() || txfRecipieName.getText().isEmpty()) 
 			return;
-
-		if ( GPingiridiant.getRowCount() ==2) {
-			if(!checkGPingiridiant(4,8))
+		if ( GPingiridiant.getRowCount() == 2) {
+			if(!checkGPingiridiant(4,8)) // is it always GPIngredient(4,8)?
 				return;
 		}
 		else {
@@ -93,9 +68,8 @@ public class AddRecipePage extends Page implements Initializable {
 			if(!checkGPingiridiant(4,allIndexes))
 				return;
 		}
-		
 		if ( GPinstruction.getRowCount() ==2) {
-			if(!checkGPinstruction(3,4))
+			if(!checkGPinstruction(3,4)) // is it always GPIngredient(3,4)?
 				return;
 		}
 		else {
@@ -103,14 +77,11 @@ public class AddRecipePage extends Page implements Initializable {
 			if(!checkGPinstruction(3,allIndexes))
 				return;
 		}
-			
 		// add recipe
-		
-		
 	}
 
 
-	private boolean checkGPingiridiant(int start , int end) {
+	private boolean checkGPingredient(int start, int end) {
 		for (int i =start ; i < end; i++) {
 			if(GPingiridiant.getChildren().get(i) instanceof TextField ) {
 				if(((TextField)GPingiridiant.getChildren().get(i)).getText().isBlank()) {
@@ -127,9 +98,7 @@ public class AddRecipePage extends Page implements Initializable {
 		return true;
 	}
 	
-	
-
-	private boolean checkGPinstruction(int start , int end) {
+	private boolean checkGPinstruction(int start, int end) {
 		for (int i = start; i < end; i++) {
 			if(GPinstruction.getChildren().get(i) instanceof TextField ) {
 				if(((TextField)GPinstruction.getChildren().get(i)).getText().isBlank()) {
@@ -139,36 +108,4 @@ public class AddRecipePage extends Page implements Initializable {
 		}
 		return true;
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
